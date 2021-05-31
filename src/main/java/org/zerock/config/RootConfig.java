603f499a86cef10d34d9871bf124993a6e870672
yjs037 <dolph037@gmail.com>
2021-06-01 02:30:50 +0900

    Spring_Controller_Java

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -2,7 +2,10 @@ package org.zerock.config;
 
 import org.springframework.context.annotation.Configuration;
 
+
+
 @Configuration
 public class RootConfig {
-
+	Character ch = (Character)'x';
+	char c = (char)ch;
 }
